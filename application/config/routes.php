308d48e42fe72c6a5534cc4641f2bb7b3f67a6cb
jyotics <jyotics09@gmail.com>
2020-03-18 17:39:07 +0530

    Add Routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = '';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+/* Routes*/
+
+$route['api/user'] = 'welcome/get_user';
+$route['api/user/create'] = 'welcome/add_user';
+
